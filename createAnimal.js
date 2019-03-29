d72aa375c110f128ed77321306be1c9a69e6555e
Jordan <jordanashment@gmail.com>
2019-03-29 15:15:44 -0600

    fixing createAnimal

--- createAnimal.js
+++ createAnimal.js
@@ -1,25 +1,44 @@
+// const { prisma } = require('./generated/prisma-client')
+
+// // A `main` function so that we can use async/await
+// async function main() {
+
+//   // Read the previously created user from the database and print their posts to the console
+//   const newUser = await prisma
+//     .createUser({
+//       name: "Bob",
+//       email: "bob@email.io",
+//       posts: {
+//         create: [{
+//           title: "Join us for GraphQL Conf in 2019",
+//           comments: {
+//             create: [{bodyText: 'I love GraphQL'}, {bodyText: 'I loved GraphQL'}]
+//           },
+//         }, {
+//           title: "Subscribe to GraphQL Weekly for GraphQL news",
+//         }]
+//       },
+//     })
+//   console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
+
+// }
+
+// main().catch(e => console.error(e))
+
 const { prisma } = require('./generated/prisma-client')
+const {pokedata} = require('./data/animal-data')
 
 // A `main` function so that we can use async/await
 async function main() {
-
-  // Read the previously created user from the database and print their posts to the console
-  const newUser = await prisma
-    .createUser({
-      name: "Bob",
-      email: "bob@email.io",
-      posts: {
-        create: [{
-          title: "Join us for GraphQL Conf in 2019",
-          comments: {
-            create: [{bodyText: 'I love GraphQL'}, {bodyText: 'I loved GraphQL'}]
-          },
-        }, {
-          title: "Subscribe to GraphQL Weekly for GraphQL news",
-        }]
-      },
+pokedata.forEach(async (animal) => {
+    const newAnimal = await prisma
+    .createPokemon({
+      name: animal.name,
+      description: animal.description,
+      url: animal.url,
     })
-  console.log(`Created new user: ${newUser.name} (ID: ${newUser.id})`)
+})
+  console.log(`Created new animal: ${newAnimal.name} (ID: ${newAnimal.id})`)
 
 }
 
