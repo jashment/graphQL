50fdf1325dbc8f86f25b13f2741382bfa35ea28d
Jordan <jordanashment@gmail.com>
2019-03-30 11:43:06 -0600

    Generating new prisma deploy

--- generated/prisma-client/index.js
+++ generated/prisma-client/index.js
@@ -7,58 +7,6 @@ var models = [
   {
     name: "Animal",
     embedded: false
-  },
-  {
-    name: "User",
-    embedded: false
-  },
-  {
-    name: "Post",
-    embedded: false
-  },
-  {
-    name: "Comment",
-    embedded: false
-  },
-  {
-    name: "Pokemon",
-    embedded: false
-  },
-  {
-    name: "Ability",
-    embedded: false
-  },
-  {
-    name: "Form",
-    embedded: false
-  },
-  {
-    name: "GameIndex",
-    embedded: false
-  },
-  {
-    name: "Item",
-    embedded: false
-  },
-  {
-    name: "Move",
-    embedded: false
-  },
-  {
-    name: "Species",
-    embedded: false
-  },
-  {
-    name: "Sprite",
-    embedded: false
-  },
-  {
-    name: "Stat",
-    embedded: false
-  },
-  {
-    name: "Type",
-    embedded: false
   }
 ];
 exports.Prisma = prisma_lib_1.makePrismaClientClass({
